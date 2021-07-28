8b68bc1562a008ea87e099fc124e602c3e94fbcb
frederico.dib <frederico.dib@ifood.com.br>
2021-07-28 20:42:40 -0300

    projetoi

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+//   parser: 'babel-eslint',
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 12,
+//     sourceType: 'module',
+//   },
+//   plugins: ['react', 'prettier'],
+//   rules: {
+//     'prettier/prettier': 1,
+//     'react/state-in-constructor': 0,
+//   },
+// };
